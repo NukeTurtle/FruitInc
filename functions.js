f09a125c4fb26f6da77a148c034dceeca0748370
NukeTurtle <stiglic.elvis@gmail.com>
2024-07-16 12:58:13 +0100

    calculate buy difference

--- functions.js
+++ functions.js
@@ -27,16 +27,18 @@ fruitButtons.forEach(button => {
         const clickName = event.target.classList[1];
         const fruit = fruits.filter((fru) => fru.name == clickName)[0];
         let fruitDiv = document.getElementById(fruit.name);
+        const multiplierDiv = button.parentElement.childNodes[3].childNodes[1].childNodes[3];
+        console.log(multiplierDiv.innerHTML)
         console.log("selected fruit:", fruit)
-        console.log(button)
+        console.log("button: ",button)
         let multiplier = fruit.cost;
         if(clickName == fruit.name) {
             button.parentElement.style.cursor = "not-allowed";
             button.style.pointerEvents = "none";
             button.style.borderColor = "red";
-            const nthDifference = Math.round((3.74 + (0.01 * (multiplier * multiplier)) + (0.25 * multiplier) + Number.EPSILON) * 100) / 100;
-            fruit.cost = nthDifference;
-            console.log(nthDifference)
+            let nthDifference = Math.round((3.74 + (0.01 * (multiplier * multiplier)) + (0.25 * multiplier) + Number.EPSILON) * 100) / 100;
+            multiplierDiv.innerHTML = Math.round(1.25 * (Number(multiplierDiv.innerHTML) + nthDifference) * 100) / 100;
+            console.log("nthDifference: ",nthDifference)
             let duration = fruit.time;
             fruitDiv.style.animationDuration = duration + "s";
             fruitDiv.classList.add("loading", "load_fruit");
@@ -75,21 +77,32 @@ buyFruitButtons.forEach(button => {
     })
 })
 
+
+function getCurrentFruit(selectedFruit) {
+    console.log("FRUIT: ",fruits.filter((fruit) => fruit.name == selectedFruit)[0]);
+}
+
 function fruitManager(fruitName) {
+    console.log("fruitName ", fruitName)
     let fruitButton = document.querySelector("." + fruitName);
-    console.log(fruitName.index)
-    const fruit = fruits.filter((fru) => fru.name)[0];
-    for (i=0;i<3;i++) {
-        console.log(fruitButton, fruitName, fruit)
-        fruitButton.click();
-        fruit.cost = fruit.cost + ((fruit.cost * 20) / 100);
-        fruit.money = fruit.money + ((fruit.money * 2) / 100);
-        fruit.time = fruit.time - ((fruit.time * 2) / 100);
-        console.log("fruit.cost: ",fruit.cost);
-        console.log("fruit.money: ",fruit.money);
-        console.log("fruit.time: ",fruit.time);
-        sleep((fruit.time * 1000));
-    }
+    console.log("fruitsL ", fruits)
+    const fruit = fruits.filter((fru) => fru.name == fruitName)[0];
+    console.log("fruit: ", fruit)
+    console.log("fruit name: ", fruit.name)
+    let secondsDiv = document.querySelectorAll(".time:has(#${fruitName})");
+    secondsDiv.innerHTML = fruit.time;
+    console.log("secondsDiv: ",secondsDiv)
+    // for (i=0;i<2;i++) {
+    //     console.log(fruitButton, fruitName, fruit)
+    //     fruitButton.click();
+    //     fruit.cost = fruit.cost + ((fruit.cost * 20) / 100);
+    //     fruit.money = fruit.money + ((fruit.money * 2) / 100);
+    //     fruit.time = fruit.time - ((fruit.time * 2) / 100);
+    //     console.log("fruit.cost: ",fruit.cost);
+    //     console.log("fruit.money: ",fruit.money);
+    //     console.log("fruit.time: ",fruit.time);
+    //     sleep((fruit.time * 1000));
+    // }
 
     // const fruitManagerButtons = document.querySelectorAll(".fruit");
     // fruitManagerButtons.forEach(button => {
@@ -105,14 +118,6 @@ function fruitManager(fruitName) {
     //     })
     // })
 }
-
-async function testFunc() {
-    console.log("demo");
-    await sleep(5000);
-    console.log("test");
-}
-
-
 
 
 
